fab1944eb22e70e0ff197f0d260881758ce59c7a
NelNawe <47140801+NelNawe@users.noreply.github.com>
2020-02-29 18:45:59 +0100

    Add files via upload

---
+++ VNext.BienEtreAuTravail.DTO/EmployeeDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VNext.BienEtreAuTravail.DTO
+{
+    class EmployeeDTO
+    {
+        public int IdEmployee { get; set; }
+        public string Pseudo { get; set; }
+    }
+}
